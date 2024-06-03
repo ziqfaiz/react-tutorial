9471a56da12840b0ff35cac1b00a4f86bc6d03ef
Haziq Faiz <haziqfaiz.ripin@gmail.com>
2024-06-03 17:07:09 +0800

    abandon this

--- tictactoe-next/app/page.tsx
+++ tictactoe-next/app/page.tsx
@@ -1,38 +1,26 @@
-import Image from "next/image";
+"use client";
 
-export default function Board() {
+interface MyButtonProps {
+  title: string;
+  disabled: boolean;
+}
+
+function MyButton({ title, disabled }: MyButtonProps) {
   return (
-    <main>
-      <div class="">
-        <Square prop="1" />
-        <Square prop="2" />
-        <Square prop="3" />
-      </div>
-      <div class="">
-        <Square prop="1" />
-        <Square prop="2" />
-        <Square prop="3" />
-      </div>
-      <div class="">
-        <Square prop="1" />
-        <Square prop="2" />
-        <Square prop="3" />
-      </div>
-    </main>
+    <button
+      disabled={disabled}
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {title}
+    </button>
   );
 }
 
-function Square({ prop }: { prop: string }) {
-  function handleClick() {
-    console.log("Click");
-  }
+export default function MyApp() {
   return (
-    <button
-      type="button"
-      class="text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-4 font-medium text-sm px-5 py-2.5 mb-2 border border-1 border-black"
-      onclick={handleClick()}
-    >
-      {prop}
-    </button>
+    <div>
+      <h1>Hi</h1>
+      <MyButton title="Button" disabled={true} />
+    </div>
   );
 }
